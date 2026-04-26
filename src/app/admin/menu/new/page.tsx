bb88eeb30de861dba86f5779b59501a7cc1b1cc6
mittens67 <nairmitha67@gmail.com>
2026-04-26 15:07:46 +0530

    build again

--- src/app/admin/menu/new/page.tsx
+++ src/app/admin/menu/new/page.tsx
@@ -2,6 +2,8 @@ import prisma from "@/lib/prisma";
 import { addProduct } from "../../actions";
 import ProductForm from "../ProductForm";
 
+export const dynamic = "force-dynamic";
+
 export default async function NewProductPage() {
   const [allPackageSizes, allFlavorOptions] = await Promise.all([
     prisma.package_sizes.findMany(),
